809a85345c2a82e35dfb09b386bb6a4c5cc5ee60
regnat <rg@regnat.ovh>
2017-10-29 22:31:30 +0100

    Typer: make recursion explicit

--- src/Typer/Infer.hs
+++ src/Typer/Infer.hs
@@ -3,6 +3,7 @@ module Typer.Infer where
 import Data.Fix (cata)
 import Data.Functor.Compose
 
+import Data.Fix (Fix(Fix))
 import qualified NixLight.Ast as NL
 import qualified NixLight.WithLoc as WL
 import qualified Typer.Environ as Env
@@ -10,11 +11,10 @@ import qualified Types
 import qualified Types.Singletons as S
 
 expr :: Env.T -> NL.ExprLoc -> Types.T
-expr _env = cata phi where
-  phi (Compose (WL.T _loc descr)) =
-    case descr of
-        (NL.Econstant c) -> constant c
-        _ -> undefined
+expr env (Fix (Compose (WL.T _loc descr))) =
+  case descr of
+      (NL.Econstant c) -> constant c
+      _ -> undefined
 
 constant :: NL.Constant -> Types.T
 constant (NL.Cint i) = S.int i
