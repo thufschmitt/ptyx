3ed4bff7e394baafe0014416a3d79405e3658837
regnat <rg@regnat.ovh>
2017-12-29 17:31:09 +0100

    Add singleton booleans

--- src/Types/Singletons.hs
+++ src/Types/Singletons.hs
@@ -2,6 +2,11 @@ module Types.Singletons where
 
 import qualified Types
 import qualified Types.Intervals as Intervals
+import qualified Types.Bool as Bool
 
 int :: Integer -> Types.T
 int i = Types.int $ Intervals.bounded i i
+
+bool :: Bool -> Types.T
+bool True = Types.bool Bool.TrueT
+bool False = Types.bool Bool.FalseT
