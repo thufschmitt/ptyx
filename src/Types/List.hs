1e399194fee5a104d61d17a4b3e24f9569e21a8b
regnat <rg@regnat.ovh>
2018-06-22 08:54:49 +0200

    feat: add list type

---
+++ src/Types/List.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Types.List
+  ( T(..)
+  , atomic
+  ) where
+
+import qualified Data.Bool.Applicative as ABool
+import           Data.Semigroup ((<>))
+import qualified Data.Set as Set
+import qualified Text.ShowM as ShowM
+import qualified Types.Bdd as Bdd
+import qualified Types.Node as Node
+import           Types.SetTheoretic
+
+data Atom t = Atom { first, second :: t }
+  deriving (Eq, Ord)
+
+instance ShowM.ShowM m t => ShowM.ShowM m (Atom t) where
+  showM (Atom hd tl) = do
+    prettyT1 <- ShowM.showM hd
+    prettyT2 <- ShowM.showM tl
+    pure $ "(" <> prettyT1 <> ")::(" <> prettyT2 <> ")"
+
+newtype T t = T (Bdd.T (Atom t))
+  deriving (Eq, Ord, SetTheoretic_)
+
+instance ShowM.ShowM m t => ShowM.ShowM m (T t) where
+  showM (T x) = do
+    prettyX <- ShowM.showM x
+    case prettyX of
+      "⊥" -> pure "⊥"
+      tt -> pure $ "(" <> tt <> ") & [⊤*]"
+
+-- | Build an atomic list type
+atomic :: t -> t -> T t
+atomic hd tl = T (Bdd.atom $ Atom hd tl)
+
+isEmptyL
+  :: forall c t m.
+     (SetTheoretic c t, c m, Applicative m)
+  => T t
+  -> m Bool
+isEmptyL (T l)
+  | Bdd.isTriviallyEmpty l = pure True
+  | Bdd.isTriviallyFull l = pure False
+  | otherwise =
+    let pairs = Bdd.toDNF l in
+    ABool.all emptyIntersect pairs
+  where
+    emptyIntersect :: (Set.Set (Atom t), Set.Set (Atom t)) -> m Bool
+    emptyIntersect (posAtoms, negAtoms) =
+      let
+        capFirsts = capN . Set.map first
+        capSeconds = capN . Set.map second
+        cupFirsts = cupN . Set.map first
+        cupSeconds = cupN . Set.map second
+      in
+      (capFirsts posAtoms `sub` cupFirsts negAtoms) ABool.&&
+      (capSeconds posAtoms `sub` cupSeconds negAtoms) ABool.&&
+        forallStrictSubset
+          (\subset compl ->
+            (capFirsts posAtoms `sub` cupFirsts subset) ABool.||
+            (capSeconds posAtoms `sub` cupFirsts compl)
+          )
+          negAtoms
+
+instance SetTheoretic Node.MemoMonad t => SetTheoretic Node.MemoMonad (T t) where
+  isEmpty = isEmptyL
--- test/SubtypingSpec.hs
+++ test/SubtypingSpec.hs
@@ -14,6 +14,7 @@ import qualified Text.Trifecta.Delta as TfDelta
 import qualified Types
 import qualified Types.Arrow as Arrow
 import qualified Types.FromAnnot as FromAnnot
+import qualified Types.List as List
 import           Types.SetTheoretic
 import qualified Types.Singletons as Singleton
 
@@ -31,12 +32,14 @@ parseType typeStr =
     FromHNix.runConvertMonad def 0 $
     FromAnnot.parseNode def annot
 
+one = Singleton.int 1
+
 spec :: Spec
 spec = do
   describe "Same-kind subtyping" $ do
     describe "integer" $ do
-      it "1<:Any" $ Singleton.int 1 <: Types.int full
-      it "1/<:2" $ not $ Singleton.int 1 <: Singleton.int 2
+      it "1<:Any" $ one <: Types.int full
+      it "1/<:2" $ not $ one <: Singleton.int 2
     describe "boolean" $ do
       it "true<:Any" $ Singleton.bool True <: Types.bool full
       it "true/<:false" $ not $ Singleton.bool True <: Singleton.bool False
@@ -46,12 +49,21 @@ spec = do
         Arrow.atom (full :: Types.T) empty <: Arrow.atom full full
       it "domain-contravariant" $
         Arrow.atom (full :: Types.T) empty <: Arrow.atom empty empty
+    describe "pairs" $ do
+      it "(1,1)<:(1,1)" $
+        List.atomic one one <: List.atomic one one
+      it "(1,1)<:(1,Int)" $
+        List.atomic one one <: List.atomic one (Types.int full)
+      it "(1,1)<:(Int,1)" $
+        List.atomic one one <: List.atomic (Types.int full) one
+      it "(Int, 1)</:(1, 1)" $ not $
+        List.atomic (Types.int full) one <: List.atomic one one
   describe "Inter-kind" $ do
     it "Empty<:Any" $ (empty :: Types.T) <: full
     it "Any</:Empty" $ not $ (full :: Types.T) <: empty
-    it "bool-int" $ not $ Singleton.int 1 <: Types.bool full
+    it "bool-int" $ not $ one <: Types.bool full
     it "int-bool" $ not $ Singleton.bool True <: Types.int full
-    it "int-arrow" $ not $ Singleton.int 1 <: Types.arrow full
+    it "int-arrow" $ not $ one <: Types.arrow full
   describe "Recursive" $ do
     it "Infinite right" $ parseType "Int" <: parseType "X where X = Int | X -> Int"
     it "Infinite right" $ parseType "X where X = Int | X -> Int" <: parseType "Int | Empty -> Int"
